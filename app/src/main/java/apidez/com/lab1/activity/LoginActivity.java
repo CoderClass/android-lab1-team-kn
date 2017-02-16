a806ad62f71053fc22683e982373e0cef6e3e842
Ke Nguyen Phu <kenguyen276@gmail.com>
2017-02-16 19:58:55 +0700

    Find view and trigger listener

--- app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
+++ app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
@@ -1,18 +1,39 @@
 package apidez.com.lab1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import apidez.com.lab1.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etEmail, etPassword;
+    private TextView tvSignUp, tvForgotPassword;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // TODO: Insert your code
+
+        etEmail = (EditText) findViewById(R.id.edtEmail);
+        etPassword = (EditText) findViewById(R.id.edtPass);
+        tvSignUp = (TextView) findViewById(R.id.tv_sign_up);
+        tvForgotPassword = (TextView) findViewById(R.id.tv_forgot);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, StreamActivity.class));
+            }
+        });
     }
 
     private void showToast(String message) {
